a83c63ff39df4f6e2ed92347fd75c36062cb0c25
Renan Joppert <renanjoppert@outlook.com>
2022-12-28 21:51:35 -0300

    Criando nosso CRUD de Ordem de Serviços~

--- app/Http/Controllers/OrdemServicoController.php
+++ app/Http/Controllers/OrdemServicoController.php
@@ -2,62 +2,106 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OrdemServico;
 use Illuminate\Http\Request;
 
 class OrdemServicoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $data = OrdemServico::simplePaginate(10);
+
+        return response()->json($data);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $data = OrdemServico::create(
+            [
+                'nome' => $request->input('nome'),
+                'descricao' => $request->input('descricao'),
+                'valor' => $request->input('valor'),
+                'servico_id' => $request->input('servico_id'),
+                'cliente_id' => $request->input('cliente_id'),
+            ]
+        );
+
+        if ($data == null) {
+            return response()->json([
+                'msg' => 'Erro ao inserir o registro'
+            ], 400);
+        }
+
+        return response()->json([
+            'data' => $data
+        ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $data = OrdemServico::where('id', $id)->first([
+            'nome',
+            'descricao',
+            'valor',
+            'servico_id',
+            'cliente_id', 'created_at'
+        ]);
+
+        if ($data == null) {
+            return response()->json([
+                'msg' => 'Erro ao encontrar o registro'
+            ], 404);
+        }
+
+        return response()->json([
+            'data' => $data
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $data = OrdemServico::where('id', $id)->first();
+
+        if ($data == null) {
+            return response()->json([
+                'msg' => 'Erro ao encontrar o registro'
+            ], 404);
+        }
+
+        $dataRequest = [
+            'nome' => $request->input('nome'),
+            'descricao' => $request->input('descricao'),
+            'valor' => $request->input('valor'),
+            'servico_id' => $request->input('servico_id'),
+            'cliente_id' => $request->input('cliente_id'),
+        ];
+        $data->update($dataRequest);
+
+        return response()->json([
+            'data' => $data
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $data = OrdemServico::where('id', $id)->first();
+
+        if ($data == null) {
+            return response()->json([
+                'msg' => 'Erro ao encontrar o registro'
+            ], 404);
+        }
+
+        $resultDelete = OrdemServico::destroy($data->id);
+
+        if ($resultDelete == 0) {
+            return response()->json([
+                'msg' => 'Erro ao excluir o registro'
+            ], 400);
+        }
+
+        return response()->json([
+            'msg' => 'Registro excluído com sucesso'
+        ]);
     }
 }
